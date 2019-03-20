4890c4be7358fc7d73b1fa298c41e5ddcccbb2f0
alumno <alumno>
2019-03-19 22:18:54 -0300

    ejercicio 07

--- ejercicios/ejercicios/ejercicios/Program.cs
+++ ejercicios/ejercicios/ejercicios/Program.cs
@@ -16,9 +16,14 @@ namespace ejercicios
       //    Console.ReadKey();
       //    Ejercicio_02();
       //    Console.ReadKey();
-      Ejercicio_03();
+      // Ejercicio_03();
+       Ejercicio_04();
+      // Console.ReadKey();
+      // Ejercicio_05();
+      //Ejercicio_06();
+      //Ejercicio_07();
       Console.ReadKey();
-        }
+    }
         static void Ejercicio_01(out int max,out int min,out int promedio)
         {
             int i;
@@ -98,8 +103,134 @@ namespace ejercicios
       }
 
     }
+    static void Ejercicio_04()
+    {
+      int i,acumulador;
+      int j;
+      int success = 0;
+      Console.WriteLine("\nNUMEROS PERFECTOS: ");
+      for (i =1; success<4; i++ )
+      {
+        acumulador = 0;
+        for (j = i - 1; j > 0; j--)
+        {
+          if(i%j == 0)
+          {
+            acumulador += j;
+          }
+        }
+        if (acumulador == i)
+        {
+          success++;
+          Console.WriteLine("- {0} -", i);
+        }
+      }
 
+    }
+    static void Ejercicio_05()
+    {
+      int ingreso, i,acumuladorMenor,acumuladorMayor,j,x;
+      bool success;
+      Console.WriteLine("\nINGRESE UN NUMERO: ");
+      while (success = int.TryParse(Console.ReadLine(), out ingreso) == false || ingreso < 0)
+      {
+        Console.WriteLine("\nERROR. ¡Reingresar número! : ");
+      }
+      Console.WriteLine("\nLOS CENTROS NUMERICOS SON: ");
+      for (i = ingreso; i >1 ; i--)
+      {
+        acumuladorMenor = 0;
+        acumuladorMayor = 0; 
+        for (j = i - 1; j > 0; j--)
+        {
+          acumuladorMenor += j;
+        }
+        for (x = i + 1; acumuladorMayor <acumuladorMenor; x++)
+        {
+          acumuladorMayor += x;
+        }
+        if (acumuladorMenor == acumuladorMayor)
+        {
+          Console.WriteLine("\n- {0} -",i);
+        }
+      }
+    }
+    static void Ejercicio_06()
+    {
+      int FechaInicio,FechaFinal;
+      bool success;
+      Console.WriteLine("\nINGRESE FECHA DE INICIO: ");
+      while (success = int.TryParse(Console.ReadLine(), out FechaInicio) == false || FechaInicio <= 0)
+      {
+        Console.WriteLine("\nERROR. ¡Reingresar número! : ");
+      }
+      Console.WriteLine("\nINGRESE FECHA FINAL: ");
+      while (success = int.TryParse(Console.ReadLine(), out FechaFinal) == false || FechaFinal < FechaInicio)
+      {
+        Console.WriteLine("\nERROR. ¡Reingresar número! (debe ser mayor a la fecha de inicio ");
+      }
+      Console.WriteLine("\nAÑOS BISIESTOS EN EL MEDIO : \n");
+      while (FechaInicio < FechaFinal)
+      {
+        if ((FechaInicio%100==0 && FechaInicio % 400==0 )||FechaInicio%4==0)
+        {
+          Console.WriteLine("- {0} -", FechaInicio);
+        }
+        FechaInicio++;
+      }
+    }
+    static void Ejercicio_07()
+    {
+      DateTime localTime = DateTime.Now;
+      DateTime fechaNacimiento;
+      int mes,dia,año;
+      bool success;
+      Console.WriteLine("\nINGRESE SU FECHA DE INICIMIENTO: ");
+      Console.WriteLine("\nINGRESE EL MES: ");
+      while (success = int.TryParse(Console.ReadLine(), out mes) == false || (mes < 1 && mes > 12))
+      {
+        Console.WriteLine("\nERROR. ¡Reingresa un número valido! : ");
+      }
+      switch (mes)
+      {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+          Console.WriteLine("\nINGRESE EL DIA: ");
+          while (success = int.TryParse(Console.ReadLine(), out dia) == false || (dia <= 0 && dia < 31))
+          {
+            Console.WriteLine("\nERROR. ¡Reingresa un dia valido! : ");
+          }
+          break;
+        case 2:
+          Console.WriteLine("\nINGRESE EL DIA: ");
+          while (success = int.TryParse(Console.ReadLine(), out dia) == false || (dia <= 0 && dia < 28))
+          {
+            Console.WriteLine("\nERROR. ¡Reingresa un dia valido! : ");
+          }
+          break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+          Console.WriteLine("\nINGRESE EL DIA: ");
+          while (success = int.TryParse(Console.ReadLine(), out dia) == false || (dia <= 0 && dia < 30))
+          {
+            Console.WriteLine("\nERROR. ¡Reingresa un dia valido! : ");
+          }
+          break;
+      }
 
-
+      Console.WriteLine("\nINGRESE EL AÑO: ");
+      while (success = int.TryParse(Console.ReadLine(), out año) == false || (año <= 0 && año < 31))
+      {
+        Console.WriteLine("\nERROR. ¡Reingresa un número valido! : ");
+      }
+      Console.WriteLine(localTime.Month);
+    }
     }
 }
