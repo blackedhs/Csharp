0ca4954410cf78bb68f0f5cd363a3a5a788cbde8
alumno <alumno@utnfra.com.ar>
2019-05-13 21:18:31 -0300

    terminar el ejer 37-40-41

--- ejercicios/ejercicio 37/Centralita/Entidades/Llamada.cs
+++ ejercicios/ejercicio 37/Centralita/Entidades/Llamada.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class Class1
+    public class Llamada
     {
+      public enum TipoDeLlamada
+    {
+      Local,
+      Provincia,
+      Todas
     }
+    private float duracion;
+    private string nroDestino;
+    private string nroOrigen;
+
+    public string NroOrigen
+    {
+      get { return nroOrigen; }
+      set { nroOrigen = value; }
+    }
+
+    public string NroDestino
+    {
+      get { return nroDestino; }
+      set { nroDestino = value; }
+    }
+
+    public float Duracion
+    {
+      get { return duracion; }
+      set { duracion = value; }
+    }
+    public Llamada(float duracion,string nroDestino,string nroOrigen)
+    {
+      Duracion = duracion;
+      NroDestino = nroDestino;
+      NroOrigen = NroOrigen;
+    }
+    public string Mostrar()
+    {
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine("Duracion: " + Duracion);
+      sb.AppendLine("Numero Destino: " + NroDestino);
+      sb.AppendLine("Numero Origen " + NroOrigen);
+      return sb.ToString();
+    }
+     public int OrdenarPorDuracion(Llamada llamada1,Llamada llamada2)
+    {
+      return llamada1.Duracion > llamada2.Duracion ? 1 : -1;
+    }
+
+  }
 }
--- ejercicios/ejercicio 37/Centralita/Entidades/Local.cs
+++ ejercicios/ejercicio 37/Centralita/Entidades/Local.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-  class Local
+  public class Local : Llamada
   {
+    private float costo;
+    private float costoLlamada;
+
+    public float CostoLlamada
+    {
+      get { return costoLlamada; }
+      set { costoLlamada = value; }
+    }
+    public Local(Llamada llamada,float costo)
+    {
+
+    }
+    public Local(string origen,float duracion,string destino,float costo) : base(duracion,destino,origen)
+    {
+      this.costo = costo;
+    }
+    public string Mostrar()
+    {
+      return (base.Mostrar() + "Costo: " + this.CostoLlamada);
+    }
   }
 }
